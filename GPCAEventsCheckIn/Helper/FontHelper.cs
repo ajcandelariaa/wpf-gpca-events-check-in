5d3123f5398debbc7232773a6de5e29a9d02d674
Aj Candelaria <ajajcandelaria@gmail.com>
2024-02-08 15:58:53 +0400

    Event Model updated

--- GPCAEventsCheckIn/Helper/FontHelper.cs
+++ GPCAEventsCheckIn/Helper/FontHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using System.Reflection;
-using System.Windows;
 
 namespace GPCAEventsCheckIn.Helper
 {
--- GPCAEventsCheckIn/Model/EventModel.cs
+++ GPCAEventsCheckIn/Model/EventModel.cs
@@ -5,6 +5,11 @@
         public static string EventCategory { get; set; } = "ANC";
         public static int EventYear { get; set; } = 2023;
         public static string EventBanner { get; set; } = "Assets/Images/Banner/img_2024_GLF.jpg";
-        public static string API { get; set; } = "https://gpcaregistration.com/api/fast-track/ANC/2023";
+        public static string API { get; set; }
+
+        static EventModel()
+        {
+            API = $"https://gpcaregistration.com/api/fast-track/{EventCategory}/{EventYear}";
+        }
     }
 }
